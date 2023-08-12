f14485d1fe85fde16f2a19fec08d9185c7759886
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-12 23:05:21 +0630

    Removed previous own logic

--- controllers/static.go
+++ controllers/static.go
@@ -11,3 +11,21 @@ func StaticHandler(tpl views.Template, data interface{}) http.HandlerFunc {
 		tpl.Execute(w, data)
 	}
 }
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	data := []faq{
+		{
+			"Q: Is there a free version?",
+			"A: Yes! We offer a free trial for 30 day",
+		},
+		{
+			"Q: What are your support hours?",
+			"A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+		},
+		{
+			"Q: How do I contact support?",
+			`A: Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com`,
+		},
+	}
+
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"os"
 
@@ -12,31 +11,12 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-type faq struct {
-	Question string
-	Answer   template.HTML
-}
-
 func main() {
 	r := chi.NewRouter()
 
-	data := []faq{
-		{
-			"Q: Is there a free version?",
-			"A: Yes! We offer a free trial for 30 day",
-		},
-		{
-			"Q: What are your support hours?",
-			"A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
-		},
-		{
-			"Q: How do I contact support?",
-			`A: Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com`,
-		},
-	}
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml")), nil))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml")), nil))
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml")), data))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml")), nil))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
