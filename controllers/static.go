7f9443cd4e5666a416115aecb9f32c1aca5b8662
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-13 11:50:21 +0630

    Changed the FAQ slice to match with Jon

--- controllers/static.go
+++ controllers/static.go
@@ -19,16 +19,16 @@ func FAQ(tpl views.Template) http.HandlerFunc {
 		Answer   template.HTML
 	}{
 		{
-			"Q: Is there a free version?",
-			"A: Yes! We offer a free trial for 30 day",
+			Question: "Q: Is there a free version?",
+			Answer:   "A: Yes! We offer a free trial for 30 day",
 		},
 		{
-			"Q: What are your support hours?",
-			"A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+			Question: "Q: What are your support hours?",
+			Answer:   "A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
 		},
 		{
-			"Q: How do I contact support?",
-			`A: Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com`,
+			Question: "Q: How do I contact support?",
+			Answer:   `A: Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com`,
 		},
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
