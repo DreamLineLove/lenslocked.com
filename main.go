5e0864ca42a640427134a508508071013685c4ed
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-13 12:44:04 +0630

    WARNING huge "template" deviation from the course
    In lesson 50, Jon talks about 2 ways of working with layouts.
    The first is including the "header" and "footer" templates in every page. This is the method Jon said he would use for the rest of the course.
    The second is having a central layout and passing different "page" templates.
    
    I am going to tread dangerous waters and try to proceed using the 2nd method.

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "home.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "contact.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
