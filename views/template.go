28d9ca13afbc6b352764068b3f139caae7179c1f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-15 03:01:10 +0630

    Implemented the Template interface in controllers package to allow for agnostic implementation (and future expansion) and to avoid potential cyclic dependencies

--- views/template.go
+++ views/template.go
@@ -8,6 +8,10 @@ import (
 	"net/http"
 )
 
+type Template struct {
+	htmlTpl *template.Template
+}
+
 func Must(t Template, err error) Template {
 	if err != nil {
 		panic(err)
@@ -29,10 +33,6 @@ func Parse(filepath string) (Template, error) {
 		return Template{}, fmt.Errorf("when parsing template: %w", err)
 	}
 	return Template{htmlTpl: tpl}, nil
-}
-
-type Template struct {
-	htmlTpl *template.Template
 }
 
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
