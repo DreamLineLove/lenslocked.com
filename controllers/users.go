c0fc1dd2774d6e53049dee66c78969f9d72eb7e9
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-15 03:42:33 +0630

    Added the users.Create method to handle the signup form

--- controllers/users.go
+++ controllers/users.go
@@ -16,7 +16,6 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Email address: %v\n", r.FormValue("email_address"))
+	fmt.Fprintf(w, "Email: %v\n", r.FormValue("email"))
 	fmt.Fprintf(w, "Password: %v\n", r.FormValue("password"))
-	fmt.Fprintf(w, "Something: %v\n", r.FormValue("something"))
 }
