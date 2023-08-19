c9be0574ea499052658aa0d1bb4b641517635206
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-19 10:36:05 +0630

    Established local database connection

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,44 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"html/template"
-	"net/http"
-	"os"
+	"log"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type User struct {
-	Name   string
-	Text   string
-	Html   template.HTML
-	Visits int
-}
-
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	defer db.Close()
 
-	user := User{
-		Name:   "Zwe Nyan Zaw",
-		Text:   `<script>alert("This is a very nefarious attack");</script>`,
-		Html:   `<script>alert("This is expected to run");</script>`,
-		Visits: 100,
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html, charset=utf-8")
-		err = t.Execute(w, user)
-		if err != nil {
-			panic(err)
-		}
-		err = t.Execute(os.Stdout, user)
-	})
-
-	fmt.Println("Starting the server on :3030")
-	err = http.ListenAndServe(":3030", nil)
+	err = db.Ping()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+
+	fmt.Println("Connected!")
 }
